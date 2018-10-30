f940290664b781410f16a2b762e36e371c1b2a38
Janet Lee <janetlee2016@gmail.com>
2018-10-29 21:51:15 -0400

    Getting .expo

---
+++ navigation/HomeScreenNavigator.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
+
+// import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import UserSignIn from '../screens/UserSignInScreen';
+import RestaurantSignIn from '../screens/RestaurantSignInScreen';
+import UserSignUp from '../screens/UserSignUpScreen';
+import RestaurantSignUp from '../screens/RestaurantSignUpScreen';
+import RestaurantVerification from '../screens/RestaurantVerificationScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+const UserStack = createStackNavigator({
+  UserSignIn: UserSignIn,
+});
+
+const RestaurantStack = createStackNavigator({
+  RestaurantSignIn: RestaurantSignIn,
+});
+
+const UserSignUpStack = createStackNavigator({
+  UserSignUp: UserSignUp,
+});
+
+const RestaurantSignUpStack = createStackNavigator({
+  RestaurantSignUp: RestaurantSignUp,
+});
+
+const RestaurantVerificationStack = createStackNavigator({
+  RestaurantVerification: RestaurantVerification,
+});
+
+export default createSwitchNavigator({
+  // You could add another route here for authentication.
+  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  HomeStack, 
+  UserStack, 
+  RestaurantStack,
+  UserSignUpStack, 
+  RestaurantSignUpStack,
+  RestaurantVerificationStack,
+});
