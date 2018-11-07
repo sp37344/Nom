bf0fb9b582b7f551bae93e7013e46ce06f378e3a
Janet Lee <janetlee2016@gmail.com>
2018-11-07 13:31:35 -0500

    incredible :')

--- screens/RestaurantPostScreen.js
+++ screens/RestaurantPostScreen.js
@@ -36,115 +36,63 @@ export default class RestaurantPostScreen extends React.Component {
 
   static navigationOptions = {
     title: 'Posts',
-  };
+  }
 
-  async getRestaurantAvailableList() {
-    var user = firebase.auth().currentUser;
-    var restaurant = user.email;
-    console.log('Getting restaurant available list')
-    var availableList = [];
-    var query = firebase.database().ref('food/').orderByChild("restaurant").equalTo(restaurant);
-    query.once("value")
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          console.log(childSnapshot.child("item").val());
-          console.log(childSnapshot.child("description").val());
-          console.log(childSnapshot.child("price").val());
-          console.log(childSnapshot.child("restaurant").val());
-          console.log(childSnapshot.child("dietaryRestrictions").val());
-          console.log(childSnapshot.child("cuisine").val());
+  getAvailableList() {
+    return new Promise(function (resolve, reject) {
+      var user = firebase.auth().currentUser;
+      var restaurant = user.email;
+      console.log('Getting restaurant available list')
+      var availableList = [];
+      var query = firebase.database().ref('food/').orderByChild("restaurant").equalTo(restaurant);
+      query.once("value")
+        .then(function(snapshot) {
+          snapshot.forEach(function(childSnapshot) {
+            console.log(childSnapshot.child("item").val());
+            console.log(childSnapshot.child("description").val());
+            console.log(childSnapshot.child("price").val());
+            console.log(childSnapshot.child("restaurant").val());
+            console.log(childSnapshot.child("dietaryRestrictions").val());
+            console.log(childSnapshot.child("cuisine").val());
 
-          availableList.push(
-          { item: childSnapshot.child("item").val(),
-            description: childSnapshot.child("description").val(),
-            price: childSnapshot.child("price").val(),
-            dietaryRestrictions: childSnapshot.child("dietaryRestrictions").val(),
-            cuisine: childSnapshot.child("cuisine").val()
-          });
+            availableList.push(
+            { item: childSnapshot.child("item").val(),
+              description: childSnapshot.child("description").val(),
+              price: childSnapshot.child("price").val(),
+              dietaryRestrictions: childSnapshot.child("dietaryRestrictions").val(),
+              cuisine: childSnapshot.child("cuisine").val()
+            });
 
+          })
         })
-      })
-      .then(function() {
-        console.log('AVAILABLE LIST IN FUNCTION');
-        console.log(availableList);
-        return availableList;
-      })
-      // .catch((error) => {
-      //   console.log(error)
-      // })
+        .then(function() {
+          console.log('AVAILABLE LIST IN FUNCTION');
+          console.log(availableList);
+          // return availableList;
+          resolve(availableList);
+        })
+        .catch((error) => {
+          console.log(error);
+          reject('error: ', error);
+        })
+    })
   }
-  /*
-  componentDidMount() {
-    this.getRestaurantAvailableList().then(result => {
-      this.setState({
-        availableList: result,
-        isLoading: false,
-      });
-    },
-    (error) => {
-      this.setState({
-        isLoading: true,
-        error
-      });
-    }
-  )
-}*/
 
-async componentDidMount() {
-  var list = await this.getRestaurantAvailableList();
-  console.log("hi");
-  console.log(list);
-  await this.setStateAsync({
-    availableList: list,
-    isLoading: false,
-  });
-  // this.setState({isLoading: false});
-}
-  /*
-  async componentWillMount() {
-    var list = await this.getRestaurantAvailableList();
+
+componentDidMount() {
+  this.getAvailableList().then((availableList) => {
+    console.log('promise returned')
     this.setState({
-      availableList: list
+      availableList: availableList,
+      isLoading: false
     });
-    console.log('set the new state')
-    // .then(() => {
-    //   this.setState({
-    //     availableList: list
-    //   })
-    //   console.log('set the new state')
-    // });
-  }
-*/
-  // getRestaurantAvailableList() {
-  //   var user = firebase.auth().currentUser;
-  //   var restaurant = user.email;
-  //   console.log('Getting restaurant available list')
-  //   var availableList = [];
-  //   firebase.database().ref('food/').orderByChild("restaurant").equalTo(restaurant).on("child_added", function(snapshot) {
-  //     console.log(snapshot.child("item").val());
-  //     console.log(snapshot.child("description").val());
-  //     console.log(snapshot.child("price").val());
-  //     console.log(snapshot.child("restaurant").val());
-  //     console.log(snapshot.child("dietaryRestrictions").val());
-  //     console.log(snapshot.child("cuisine").val());
-  //
-  //     availableList.push(
-  //     { item: snapshot.child("item").val(),
-  //       description: snapshot.child("description").val(),
-  //       price: snapshot.child("price").val(),
-  //       dietaryRestrictions: snapshot.child("dietaryRestrictions").val(),
-  //       cuisine: snapshot.child("cuisine").val()
-  //     });
-  //   }).then(() => {
-  //     console.log('AVAILABLE LIST IN FUNCTION');
-  //     console.log(availableList);
-  //     return availableList;
-  //   });
-  // }
+  }, (error) => {
+    alert(error);
+  })
+}
 
 
   // have a list of expiration dates (one for each quantity)
-  //
   //
   // quantity: < 0 make that be the specification for querying for the user interface
   // have an expiration date
